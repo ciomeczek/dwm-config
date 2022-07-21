ac2930a128860c9a49610b141f7522a17fafef98
ciomeczek <ciomek@tutanota.com>
2022-07-21 18:28:39 +0200

    more fixes

--- install.sh
+++ install.sh
@@ -22,8 +22,8 @@ sudo make install
 cd $HOME
 mkdir -p Repositories Aur Pictures Downloads Videos Documents Desktop
 
-cp $REPO_PATH/Pictures/lain.jpg -r $HOME/Pictures/
-cp $REPO_PATH/.oh-my-zsh $REPO_PATH/.xbindkeysrc $REPO_PATH/.zshrc $REPO_PATH/.config $REPO_PATH/.xprofile $REPO_PATH/.local -r $HOME
+cp -rf $REPO_PATH/Pictures/lain.jpg -r $HOME/Pictures/
+cp -rf $REPO_PATH/.oh-my-zsh $REPO_PATH/.xbindkeysrc $REPO_PATH/.zshrc $REPO_PATH/.config $REPO_PATH/.xprofile $REPO_PATH/.local -r $HOME
 
 echo "Reboot? y/n"
 read yn
